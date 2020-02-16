6127e8be073b7e06080acae668b08accec76c79a
Mateusz Trynkiewicz <mateusz.trynkiewicz@outlook.com>
2020-02-16 22:00:08 +0100

    dodanie dokumentacji

--- expensesMonitor/api/model/model.js
+++ expensesMonitor/api/model/model.js
@@ -17,3 +17,29 @@ var SingleExpense = thinkagain.createModel('SingleExpense',
 	required: [ 'expenseValue', 'expenseDate' ]
 });
 exports.SingleExpense = SingleExpense;
+
+var SingleCategory = thinkagain.createModel('SingleCategory', 
+{
+	type:'object',
+	properties:
+	{
+		id: { type: 'string' },
+		categoryName: { type: 'string' },
+		categoryPriority: { type: 'number' },
+	},
+	required: [ 'categoryName', 'categoryPriority' ]
+});
+exports.SingleCategory = SingleCategory;
+
+var SingleCurrency = thinkagain.createModel('SingleCurrency', 
+{
+	type:'object',
+	properties:
+	{
+		id: { type: 'string' },
+		currencyName: { type: 'string' },
+		currencyShort: { type: 'string' },
+	},
+	required: [ 'categoryName', 'categoryPriority' ]
+});
+exports.SingleCurrency = SingleCurrency;
